b4b98b850973786782a04d14cd4d80b85d3064b5
caswick <caswick@mix.wvu.edu>
2021-02-15 18:31:25 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Hello</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>Rocket League</h2>
+                    <p>Rating:</p>
+                    <p>About:</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Super Smash Bros Ultimate</h2>
+                    <p>Rating:</p>
+                    <p>About:</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>Magic Arena</h2>
+                    <p>Rating:</p>
+                    <p>About:</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>Borderlands 3</h2>
+                    <p>Rating:</p>
+                    <p>About:</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
